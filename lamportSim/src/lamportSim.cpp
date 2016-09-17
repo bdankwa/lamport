7e97e80113168026248b0f4bacbe885a54a2d177
Boakye Dankwa <bdankwa@linux-567p.site>
2016-09-17 18:37:31 -0400

    Initial eclipse C++ project

---
+++ lamportSim/src/lamportSim.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lamportSim.cpp
+// Author      : Boakye Dankwa
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
